d839811f01b04af89863d21b5c553caa1a6d0960
Alex Galinier <contact@alexgalinier.com>
2018-05-04 09:24:34 +0200

    feat(pizzas): Add new pizzas

--- src/routes/pizzas/data.js
+++ src/routes/pizzas/data.js
@@ -18,5 +18,35 @@ export const pizzas = [
       B: 19
     },
     ingredients: ['Emmental', 'Tomates', 'Jambon', 'Champignons']
+  },
+  {
+    name: 'Saumona',
+    image: 'https://pizza-122.herokuapp.com/images/pizza3.png',
+    price: {
+      S: 18,
+      N: 20,
+      B: 22
+    },
+    ingredients: ['Emmental', 'Saumon', 'Fromage']
+  },
+  {
+    name: 'Vege',
+    image: 'https://pizza-122.herokuapp.com/images/pizza1.png',
+    price: {
+      S: 10,
+      N: 11,
+      B: 12
+    },
+    ingredients: ['Emmental', 'Brocolis', 'Poireaux', 'Oignons', 'Tomates']
+  },
+  {
+    name: '4 fromages',
+    image: 'https://pizza-122.herokuapp.com/images/pizza2.png',
+    price: {
+      S: 10,
+      N: 11,
+      B: 12
+    },
+    ingredients: ['Emmental', 'Bleu', 'Cantal', 'Parmesan', 'Creme']
   }
 ];
